ed365fd689e35da01da41b033fc22224feb4db86
Nick <nick@Nicks-MacBook-Pro.local>
2026-04-28 16:44:00 +0100

    Created spring boot app

--- ad-insights-backend/src/main/java/com/example/AdInsightsBackend/AdInsightsBackendApplication.java
+++ ad-insights-backend/src/main/java/com/example/AdInsightsBackend/AdInsightsBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.AdInsightsBackend;
+package com.example.adinsightsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdInsightsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdInsightsBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdInsightsBackendApplication.class, args);
+    }
 
 }
--- ad-insights-backend/src/test/java/com/example/AdInsightsBackend/AdInsightsBackendApplicationTests.java
+++ ad-insights-backend/src/test/java/com/example/AdInsightsBackend/AdInsightsBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.AdInsightsBackend;
+package com.example.adinsightsbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AdInsightsBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
