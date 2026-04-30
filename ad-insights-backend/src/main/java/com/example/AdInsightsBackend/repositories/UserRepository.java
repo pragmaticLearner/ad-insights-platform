4df166f3d5997227ab8bfec2f35b8fc6f4b6c05e
Nick <nick@Nicks-MacBook-Pro.local>
2026-04-30 15:11:28 +0100

    Added flyway plugin

---
+++ ad-insights-backend/src/main/java/com/example/AdInsightsBackend/repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.adinsightsbackend.repositories;
+
+interface UserRepository extends org.springframework.data.repository.CrudRepository<com.example.adinsightsbackend.entities.User, java.lang.Long> {
+}
