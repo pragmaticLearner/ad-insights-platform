4762e2cdb545fd7d90d7a0b5f7222c4481c64396
Nick <nick@Nicks-MacBook-Pro.local>
2026-04-30 11:09:05 +0100

    Created docker files

---
+++ ad-insights-backend/src/main/java/com/example/AdInsightsBackend/entities/User.java
@@ -0,0 +1,23 @@
+package com.example.adinsightsbackend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+}
