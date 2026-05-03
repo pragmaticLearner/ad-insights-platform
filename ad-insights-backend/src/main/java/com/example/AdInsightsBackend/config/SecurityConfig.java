1c74541b9ac6e84008b740aa92793c3b3120c84e
Nick <nick@Nicks-MBP.home.local>
2026-05-03 21:59:53 +0100

    Created security config

--- ad-insights-backend/src/main/java/com/example/AdInsightsBackend/config/SecurityConfig.java
+++ ad-insights-backend/src/main/java/com/example/AdInsightsBackend/config/SecurityConfig.java
@@ -1,22 +1,26 @@
-package com.example.adinsightsbackend.security;
+package com.example.adinsightsbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-class SecurityConfiguration {
+public class SecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{some}thing")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/create-user").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
     }
 }
-
