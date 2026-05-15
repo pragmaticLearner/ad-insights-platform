d54c9c38d8bed5b93a1386fd9b4e637dc40852c1
Nick <nick@Nicks-MacBook-Pro.local>
2026-05-15 17:59:16 +0100

    Added onClick to image

--- ad-insights-frontend/src/features/ui/components/navbar/HomePageNavBar.tsx
+++ ad-insights-frontend/src/features/ui/components/navbar/HomePageNavBar.tsx
@@ -1,11 +1,19 @@
 import {HStack, Image} from "@chakra-ui/react";
 import logo from "@/assets/giraffe_logo.jpg";
 import ColourModeSwitch from "@/features/ui/components/navbar/ColourModeSwitch.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function HomePageNavBar() {
+    const navigate = useNavigate();
+
     return (
         <HStack justifyContent={"space-between"} padding={"10px"}>
-            <Image src={logo} boxSize={"60px"}/>
+            <Image
+                src={logo}
+                boxSize={"60px"}
+                onClick={() => navigate(import.meta.env.VITE_HOME_URL)}
+                cursor={"pointer"}
+            />
             <ColourModeSwitch />
         </HStack>
     );
